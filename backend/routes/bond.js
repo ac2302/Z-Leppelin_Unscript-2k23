685a18401554ea7d4bc10291f720ccfb20897132
Aryan Chavan <aryan@aryansdomain.com>
2023-02-12 06:12:06 +0530

    add route to get quantity of owned bonds

--- backend/routes/bond.js
+++ backend/routes/bond.js
@@ -1,9 +1,25 @@
+const authOnlyMiddleware = require("../middlewares/authOnly");
 const Bond = require("../models/Bond");
-
+const Order = require("../models/Order");
 const router = require("express").Router();
 
 router.get("/", async (req, res) => {
 	res.json(await Bond.find());
+});
+
+router.get("/my-count/:id", authOnlyMiddleware([]), async (req, res) => {
+	const orders = await Order.find({
+		user: req.auth.user,
+		bond: req.params.id,
+		completed: true,
+	});
+
+	let count = 0;
+	orders.forEach((order) => {
+		if (order.type == "buy") count++;
+		else count--;
+	});
+	res.json({ count });
 });
 
 module.exports = router;
